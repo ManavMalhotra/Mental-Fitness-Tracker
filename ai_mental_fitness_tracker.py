970c5f8efbc9ae829f6b9c869d2a4f3969da57f7
Manav Malhotra <manavmalhotrafrnd4u@gmail.com>
2023-11-02 21:40:56 +0530

    optimize frontend

--- ai_mental_fitness_tracker.py
+++ ai_mental_fitness_tracker.py
@@ -8,7 +8,6 @@ import plotly.express as px
 from sklearn.preprocessing import LabelEncoder
 
 
-"""# Preparing and Loading the data"""
 
 df2 = pd.read_csv("mental-and-substance-use-as-share-of-disease.csv")
 df1 = pd.read_csv("prevalence-by-mental-and-substance-use-disorder.csv")
@@ -20,37 +19,15 @@ df2.head(10)
 data = pd.merge(df1,df2)
 data.head(10)
 
-"""# Data Cleaning"""
 
 data.isnull().sum()
 
 data.drop('Code', axis=1, inplace=True)
 data.head(10)
 
-# data.size,data.shape
-
 data = data.set_axis(["Country","Year","Schizophrenia","Bipolar-disorder","Eating-disorders","Anxiety-disorders","Drug-use disorders","Depressive-disorders","Alcohol-use disorders","Mental-Fitness"], axis ='columns')
 data.head(10)
 
-# """#Visualization"""
-
-# plt.figure(figsize=(12,6))
-# sb.heatmap(data.corr(),annot=True,cmap='Reds')
-# plt.plot()
-
-# sb.pairplot(data,corner=True)
-# plt.show()
-
-# mean = data["Mental-Fitness"].mean()
-# mean
-
-# Pie = px.pie(data, values="Mental-Fitness", names='Year')
-# Pie.show()
-
-# fig = px.line(data, x='Year', y = 'Mental-Fitness', color = 'Country', markers=True ,color_discrete_sequence=['red','blue'], template='plotly_dark')
-# fig.show()
-
-# data.info()
 from sklearn.preprocessing import LabelEncoder
 
 lab = LabelEncoder()
@@ -58,7 +35,6 @@ for i in data.columns:
   if data[i].dtype == 'object':
     data[i] = lab.fit_transform(data[i])
 
-# data.shape
 
 """#Split Data"""
 
@@ -74,10 +50,6 @@ print()
 print("ytrain =", ytrain.shape)
 print("ytest =", ytest.shape)
 
-"""#Model Training
-
-## **1)Linear Regression**
-"""
 
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
@@ -95,7 +67,6 @@ print("MSE is {}".format(mse))
 print("RMSE is {}".format(rmse))
 print("R2 score is {}".format(r2))
 
-"""## **2)Random Forest Regression**"""
 
 from sklearn.ensemble import RandomForestRegressor
 rf = RandomForestRegressor()
@@ -112,7 +83,6 @@ print("MSE is {}".format(mse))
 print("RMSE is {}".format(rmse))
 print("R2 score is {}".format(r2))
 
-"""## **3)SVM Regression**"""
 
 from sklearn.svm import SVR
 svr=SVR()
@@ -129,7 +99,6 @@ print("MSE is {}".format(mse))
 print("RMSE is {}".format(rmse))
 print("R2 score is {}".format(r2))
 
-"""#Evaluation"""
 
 ytest_pred = lr.predict(xtest)
 mse = mean_squared_error(ytest,ytest_pred)
@@ -169,10 +138,7 @@ print("MSE is {}".format(mse))
 print("RMSE is {}".format(rmse))
 print("R2 score is {}".format(r2))
 
-"""#Result"""
 
-# print("------  Welcome to Mental Fitness Tracker ------")
-# print("------ Please Enter the Following Details ------ \n")
 
 import streamlit as st
 
@@ -181,8 +147,8 @@ st.write("------ Please Enter the Following Details ------ \n")
 
 Country= st.text_input('Enter Your country Name: ')
 Year= st.number_input("Enter the Year:",min_value=0.0, format="%.2f")
-Schizophrenia = st.number_input("Schizophrenia rate in %: ", min_value=0.0,format="%.2f")
-Bipolar= st.number_input("Bipolar disorder rate in %: ", min_value=0.0,format="%.2f")
+Schizophrenia = 78
+Bipolar= 69
 Eating= st.number_input("Eating disorder rate in %: ",min_value=0.0, format="%.2f")
 Anxiety= st.number_input("Anxiety rate in %: ", min_value=0.0,format="%.2f")
 Drug_use= st.number_input("Drug Usage rate in per year %: ",min_value=0.0, format="%.2f")
@@ -193,14 +159,11 @@ Alcohol= st.number_input("Alcohol Consuming rate per year in %: ",min_value=0.0,
 
 
 if st.button("Calculate"):
-    st.write("Button was clicked!")
     Country = lab.fit_transform([Country])
     inputData = pd.DataFrame({'Country':Country,'Year':Year,'Schizophrenia':Schizophrenia,'Bipolar-disorder':Bipolar,'Eating disorders':Eating,'Anxiety-disorders':Anxiety,'Drug-use disorders':Drug_use,'Depressive-disorders':Depression,'Alcohol-use disorders':Alcohol},index=[0])
     prediction = rf.predict(inputData.values)
 
-    # prediction= rf.predict([Country,Year,Schizophrenia,Bipolar,Eating,Anxiety,Drug_use,Depression,Alcohol])
     st.write("Your Mental Fitness is {}%".format(prediction*10))
 
-    # inputData = inputData.reshape(-1,)
 
 
